b8e082427d341647c6459706096fae10abd9274a
W1llowTree <frankhuang132@gmail.com>
2023-11-08 17:57:24 -0500

    Changed arm code, need to config robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Arm.java
@@ -2,46 +2,55 @@ package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class Arm {
+public class
+Arm {
+    final static double WRIST_OPEN = 0.8;
+    final static double WRIST_CLOSED = 0.2;
     final static double CLAW_OPEN = 0.8;
     final static double CLAW_CLOSED = 0.2;
 
     OpMode opMode;
 
-    DcMotor elbowMotor;
+    DcMotor elbowMotorLeft, elbowMotorRight;
     Servo wristServo;
     Servo clawServo;
 
     public Arm(OpMode opMode) {
         this.opMode = opMode;
 
-        elbowMotor = opMode.hardwareMap.get(DcMotor.class, "a");
-        clawServo = opMode.hardwareMap.get(Servo.class, "b");
+        elbowMotorLeft = opMode.hardwareMap.get(DcMotor.class, "elbowLeft");
+        elbowMotorRight = opMode.hardwareMap.get(DcMotor.class, "elbowRight");
 
+        wristServo = opMode.hardwareMap.get(Servo.class, "wrist");
+        clawServo = opMode.hardwareMap.get(Servo.class, "claw");
+
+        elbowMotorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         opMode.telemetry.addData("Arm: ", "Initialized");
     }
 
     public void update() {
         // rotate whole arm (elbow joint motor)
-        if (opMode.gamepad1.a) { // a and b buttons for now
-            elbowMotor.setPower(0.5);
-        } else if (opMode.gamepad1.b) {
-            elbowMotor.setPower(-0.5);
-        } else {
-            elbowMotor.setPower(0);
+        elbowMotorLeft.setPower(opMode.gamepad1.left_stick_y);
+        elbowMotorRight.setPower(opMode.gamepad1.left_stick_y);
+
+        // Wrist
+        if(opMode.gamepad1.dpad_up) {
+            // move to 0 degrees.
+            clawServo.setPosition(WRIST_OPEN);
+        } else if (opMode.gamepad1.dpad_down) {
+            // move to 90 degrees.
+            clawServo.setPosition(WRIST_CLOSED);
         }
 
-        // open and close claw; x and y buttons for now
-        if(opMode.gamepad1.y) {
-            // move to 0 degrees.
-            opMode.telemetry.addData("Key", "y");
-            clawServo.setPosition(CLAW_OPEN);
-        } else if (opMode.gamepad1.x) {
-            // move to 90 degrees.
-            opMode.telemetry.addData("Key", "x");
-            clawServo.setPosition(CLAW_CLOSED);
+        // Claw
+        if (opMode.gamepad1.left_trigger > 0) {
+            wristServo.setPosition(CLAW_CLOSED);
+        }
+        else if (opMode.gamepad1.right_trigger > 0) {
+            wristServo.setPosition(CLAW_OPEN);
         }
     }
 }
