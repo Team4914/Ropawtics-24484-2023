496ecff54b5cb08637f361098a8e99555d5e98c9
CLS-Ghost350 <jennifer.f.dan@gmail.com>
2023-11-07 21:50:12 -0500

    refactored mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Arm.java
@@ -2,43 +2,46 @@ package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Arm {
+    final static double CLAW_OPEN = 0.8;
+    final static double CLAW_CLOSED = 0.2;
+
+    OpMode opMode;
+
     DcMotor elbowMotor;
     Servo wristServo;
     Servo clawServo;
 
-    OpMode opMode;
     public Arm(OpMode opMode) {
         this.opMode = opMode;
 
         elbowMotor = opMode.hardwareMap.get(DcMotor.class, "a");
         clawServo = opMode.hardwareMap.get(Servo.class, "b");
+
+        opMode.telemetry.addData("Arm: ", "Initialized");
     }
 
     public void update() {
-        if (opMode.gamepad1.left_stick_y > 0) {
+        // rotate whole arm (elbow joint motor)
+        if (opMode.gamepad1.a) { // a and b buttons for now
             elbowMotor.setPower(0.5);
-        } else if (opMode.gamepad1.left_stick_y < 0) {
+        } else if (opMode.gamepad1.b) {
             elbowMotor.setPower(-0.5);
         } else {
             elbowMotor.setPower(0);
         }
 
+        // open and close claw; x and y buttons for now
         if(opMode.gamepad1.y) {
             // move to 0 degrees.
             opMode.telemetry.addData("Key", "y");
-            clawServo.setPosition(0);
-        } else if (opMode.gamepad1.x || opMode.gamepad1.b) {
+            clawServo.setPosition(CLAW_OPEN);
+        } else if (opMode.gamepad1.x) {
             // move to 90 degrees.
             opMode.telemetry.addData("Key", "x");
-            clawServo.setPosition(0.5);
-        } else if (opMode.gamepad1.a) {
-            // move to 180 degrees.
-            opMode.telemetry.addData("Key", "a");
-            clawServo.setPosition(1);
+            clawServo.setPosition(CLAW_CLOSED);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainOpMode.java
@@ -7,50 +7,20 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class MainOpMode extends OpMode {
+    MecanumDrive mecanumDrive;
     Arm arm;
-    DcMotor RFMotor;
-    DcMotor LFMotor;
-    DcMotor RBMotor;
-    DcMotor LBMotor;
-
-    public void moveDriveTrain() {
-        double vertical = 0; // Front-back motion
-        double horizontal = 0; // Left-right motion
-        double pivot = 0; // Turn angle
-
-        /*Depending on what motors are flipped, vertical must be negative or positive.
-        * Test with robot, and change to positive if necessary*/
-        vertical = -gamepad1.left_stick_y;
-        horizontal = gamepad1.left_stick_x;
-        pivot = gamepad1.right_stick_x;
-
-        // Diagonally Alternate Motors must be the same
-        /* If robot does not work properly, set the vertical values for RB and LF to positive*/
-        RFMotor.setPower(-pivot + (vertical - horizontal));
-        RBMotor.setPower(-pivot + (vertical + horizontal));
-        LFMotor.setPower(pivot + (vertical + horizontal));
-        LBMotor.setPower(pivot + (vertical - horizontal));
-    }
 
     @Override
     public void init() {
-        RFMotor = hardwareMap.get(DcMotor.class, "right_front_drive");
-        LFMotor = hardwareMap.get(DcMotor.class, "left_front_drive");
-        RBMotor = hardwareMap.get(DcMotor.class, "right_back_drive");
-        LBMotor = hardwareMap.get(DcMotor.class, "left_back_drive");
-        telemetry.addData("Hardware: ", "Initialized");
-
-        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        mecanumDrive = new MecanumDrive(this);
         arm = new Arm(this);
+
+        telemetry.addData("Hardware: ", "Initialized");
     }
 
     @Override
     public void loop() {
-        moveDriveTrain();
+        mecanumDrive.update();
         arm.update();
     }
-
-
 }
