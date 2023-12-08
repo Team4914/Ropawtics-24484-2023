b7ca19e688e142739de3fabf4eb44954e3b8168c
W1llowTree <frankhuang132@gmail.com>
2023-12-08 17:34:01 -0500

    hang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hamza/OpModes/Hanger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hamza/OpModes/Hanger.java
@@ -5,9 +5,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hanger {
-    public static double HANG_SERVO_DOWN = 0.1;
-    public static double HANG_SERVO_UP = 0.6;
+    public static double HANG_SERVO_DOWN = 0.01;
+    public static double HANG_SERVO_UP = 0.5;
 
+    double liftServoPos = 0;
 
     OpMode opmode;
 
@@ -27,12 +28,17 @@ public class Hanger {
     private void updateServo() {
         if (opmode.gamepad1.dpad_left) {
             liftServo.setPosition(HANG_SERVO_DOWN);
-            opmode.telemetry.addData("Num", HANG_SERVO_DOWN);
+            //opmode.telemetry.addData("liftServo: ", HANG_SERVO_DOWN);
+            //liftServoPos -= 0.0001;
         }
         else if (opmode.gamepad1.dpad_right) {
             liftServo.setPosition(HANG_SERVO_UP);
-            opmode.telemetry.addData("Num", HANG_SERVO_UP);
+            //opmode.telemetry.addData("liftServo: ", HANG_SERVO_UP);
+            //liftServoPos += 0.0001;
         }
+
+        //liftServo.setPosition(Math.abs(liftServoPos % 1.0));
+        //opmode.telemetry.addData("liftServo: ", liftServoPos);
     }
 
     private void updateMotor() {
